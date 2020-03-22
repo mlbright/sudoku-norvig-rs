21237e9b9299338a897577d8679c41cbaa8828dc
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2020-03-22 18:37:17 -0400

    cross product

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use itertools::iproduct;
+
+static DIGITS: &str = "12345679";
+static ROWS: &str = "ABCDEFGHI";
+
 fn main() {
     println!("Hello, world!");
+
+    for (i, j) in iproduct!(ROWS.chars(), DIGITS.chars()) {
+        println!("{}{}", i, j);
+    }
 }
