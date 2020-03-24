435554d67e7c4e4153c6218d91b60a7a14bc127c
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2020-03-24 15:33:39 -0400

    formatting

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ static DIGITS: &str = "123456789";
 static ROWS: &str = "ABCDEFGHI";
 
 fn main() {
-    let squares: Vec::<_> = cross(ROWS,DIGITS);
+    let squares: Vec<_> = cross(ROWS, DIGITS);
     assert_eq!(squares.len(), 81);
 }
 
-fn cross(a:&str,b:&str) -> Vec::<String> {
+fn cross(a: &str, b: &str) -> Vec<String> {
     let mut cp = Vec::<String>::new();
     for (i, j) in iproduct!(a.chars(), b.chars()) {
         cp.push(format!("{}{}", i, j));
