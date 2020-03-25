f713b255ea85509913989d36f40a06bc9f2b191a
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2020-03-25 00:46:12 -0400

    peers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use itertools::iproduct;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 static DIGITS: &str = "123456789";
 static ROWS: &str = "ABCDEFGHI";
@@ -43,11 +44,39 @@ fn main() {
         units.insert(s.clone(), group);
     }
 
-    println!("{:?}", units.get("C2").unwrap());
+    // println!("{:?}", units.get("C2").unwrap());
     for s in squares.iter() {
         match units.get(s) {
             None => panic!("No units for {}", s),
             Some(ulist) => assert_eq!(ulist.len(), 3),
+        }
+    }
+
+    let mut peers: HashMap<String, Vec<String>> = HashMap::new();
+
+    for s in squares.iter() {
+        let mut peer_set: HashSet<String> = HashSet::new();
+        for unit in units.get(s).unwrap() {
+            for square in unit.iter() {
+                if square != s {
+                    peer_set.insert(square.clone());
+                }
+            }
+        }
+        let mut peer_list: Vec<String> = Vec::new();
+        for peer in &peer_set {
+            peer_list.push(peer.clone());
+        }
+        peers.insert(s.clone(), peer_list);
+    }
+
+    // let mut test = peers.get("C2").unwrap().clone();
+    // test.sort();
+    // println!("{:?}", test);
+    for s in squares.iter() {
+        match peers.get(s) {
+            None => panic!("No peers for {}", s),
+            Some(plist) => assert_eq!(plist.len(), 20),
         }
     }
 }
