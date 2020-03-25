be0f2db905c85ecdaf4da62809ca6d3cd451e497
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2020-03-25 00:10:15 -0400

    units

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use itertools::iproduct;
+use std::collections::HashMap;
 
 static DIGITS: &str = "123456789";
 static ROWS: &str = "ABCDEFGHI";
@@ -17,7 +18,7 @@ fn main() {
         unitlist.push(cross(&r.to_string(), DIGITS));
     }
 
-    let rs: Vec<_> = vec!["ABC".to_string(), "CDE".to_string(), "GHI".to_string()];
+    let rs: Vec<_> = vec!["ABC".to_string(), "DEF".to_string(), "GHI".to_string()];
     let cs: Vec<_> = vec!["123".to_string(), "456".to_string(), "789".to_string()];
     for r in rs.iter() {
         for c in cs.iter() {
@@ -25,8 +26,30 @@ fn main() {
         }
     }
 
-    println!("{:?}", unitlist);
     assert_eq!(unitlist.len(), 27);
+
+    let mut units: HashMap<String, Vec<Vec<String>>> = HashMap::new();
+
+    for s in squares.iter() {
+        let mut group: Vec<Vec<String>> = Vec::new();
+        for unit in unitlist.iter() {
+            for square in unit.iter() {
+                if square == s {
+                    group.push(unit.clone());
+                    break;
+                }
+            }
+        }
+        units.insert(s.clone(), group);
+    }
+
+    println!("{:?}", units.get("C2").unwrap());
+    for s in squares.iter() {
+        match units.get(s) {
+            None => panic!("No units for {}", s),
+            Some(ulist) => assert_eq!(ulist.len(), 3),
+        }
+    }
 }
 
 fn cross(a: &str, b: &str) -> Vec<String> {
